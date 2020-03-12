8b424b3f7d40a5544a2b0a1963c0d7dbf1c39832
Puneet <puneettiwari61@gmail.com>
2020-03-12 12:43:27 +0530

    extension-final

---
+++ README.md
@@ -0,0 +1,6 @@
+
+## Steps To Run Chrome Extension 🛹
+* [ ] Open `Chrome` click on the menu on the right `setting` ➡️ `More Tools`  ➡️ `Extensions`
+* [ ] Switch On the `developer mode` on the right top corner.
+* [ ] Click on `load unpacked` on the left and select the `github-extension` folder.
+* [ ] Cheers 💐 you are done. Check it out by visiting any issues page on github.
--- main.js
+++ main.js
@@ -1,74 +1,46 @@
-// You js goes here
-// import axios from "axios"
-
-  alert('hello I m extension of chrome ' + window.location.href)
-
- 
-  // issues =  Array.from(issues)
-  // issues.shift()
-
-  // issues.forEach(i => {
-  //   i.innerText = i.innerText +  " Project" 
-  // })
-  // console.log(issues,"sadasdsa1231231")
-  // console.log(result, "resyusds")
-  // let project = document.querySelector('.f6 text-bold link-gray-dark no-underline')
- 
-  // let result = project.unshift()
-  // console.log(result,"sdasdsas")
-
-// fetch("https://github.com/repos/ayush-mann/trello-fullStack/issues/1",{
-//   method:"GET",
-//   headers:{
-//     "Content-Type":"text",
-//     "Accept":"application/vnd.github.machine-man-preview",
-//     // mode: 'cors',
-//     // credentials: 'omit'
-    
-//   }
-// }).then(res => res.text)
-// .then(res => console.log(res))
 
 
-fetch("https://github.com/ayush-mann/trello-fullStack/issues/1").then(function (response) {
-	// The API call was successful!
-	return response.text()
-}).then(function (html) {
-	// This is the HTML from our response as a text string
-  var parser = new DOMParser();
-  var doc = parser.parseFromString(html, 'text/html');
-  
-  var item = doc.querySelectorAll('.js-issue-sidebar-form');
-  let children = item[1].innerText
-  let regexElem  = children.replace(/ /g,'').trim().replace(/\r?\n|\r/g," ").split('    ')
-  let filtered = regexElem.filter(item => item.length>0)
-  filtered.shift()
-  console.log(filtered.join(" "))  
-  var issue = document.getElementById(`issue_7_link`)
-console.log(issue,"isuue gol,mal")
-issue.innerText = issue.innerText + " " + `Project ${filtered[1]}`
- 
-}).catch(err => console.log(err))
 
-
-  // content.js
-// background.js
-
-// // Called when the user clicks on the browser action.
-// chrome.browserAction.onClicked.addListener(function(tab) {
-//   // Send a message to the active tab
-//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//     var activeTab = tabs[0];
-//     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-//   });
-// });
-
-// // This block is new!
-// chrome.runtime.onMessage.addListener(
-//   function(request, sender, sendResponse) {
-//     if( request.message === "open_new_tab" ) {
-//       chrome.tabs.create({"url": request.url});
-//     }
-//   }
-// );
-
+(function (){ var issues = document.querySelectorAll('.opened-by')
+  var totalIssues = [...issues].map(j => j.innerText.trim().replace(/\n/ig, '').split(' ')[0])
+  // console.log(totalIssues)
+  function fetchURLs() {
+    {
+      totalIssues.forEach(i => {
+        var issueNumber = i.split('#').join('')
+        
+        var presentHref = window.location.href  
+        var modifiedHref = window.location.href.includes("?q=is%3Aissue+is%3Aclosed") ? presentHref.split("?")[0] : presentHref
+        fetch(modifiedHref + `/${issueNumber}`).then((response) => {
+          return response.text()
+        }).then(html => {
+          var parser = new DOMParser();
+          var doc2 = parser.parseFromString(html, 'text/html');
+          var item = doc2.querySelectorAll('.js-issue-sidebar-form');
+          let children = item[1].innerText
+          let regexElem = children.replace(/ /g, '').trim().replace(/\r?\n|\r/g, " ").split('    ')
+          let filtered = regexElem.filter(item => item.length > 0)
+          filtered.shift()
+          // console.log(filtered)
+          var issue = document.getElementById(`issue_${issueNumber}_link`)
+          // console.log(issueNumber)
+          var p = document.createElement('p')
+          // console.log(document.querySelector('.user-profile-link').innerText.split(' ').pop())
+          // console.log(modifiedHref.split('/')[3])
+          document.querySelector('.user-profile-link').innerText.split(' ').pop() == modifiedHref.split('/')[3] ? p.innerText = `${filtered.join(' ').split(' ')[2]}` : p.innerText = `${filtered.join(' ').split(' ')[1]}`
+          p.style.background="#6FCA55"
+          p.style.color = "#ffffff"
+          p.style.border="1px solid grey"
+          p.style.width = "100px"
+          p.style.fontSize = "10px"
+          p.style.borderRadius = "2px"
+          p.style.textAlign =  "center";
+          // p.classList.add('btn btn-primary')
+          p.innerText=='Noneyet' ? '' : issue.append(p)
+          
+        })
+      })
+    }
+  }
+  fetchURLs()
+})()
