79518a439443079c25fdb55ff4fc283c62c3c711
Ayush_Mann <learnertsk@gmail.com>
2020-03-15 01:36:46 +0530

    styling

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ function fetchURLs() {
   {
     totalIssues.forEach(i => {
       var issueNumber = i.split('#').join('')
-      
       var presentHref = window.location.href  
       var modifiedHref = window.location.href.includes("?q=is%3Aissue+is%3Aclosed") ? presentHref.split("?")[0] : presentHref
       fetch(modifiedHref + `/${issueNumber}`).then((response) => {
@@ -25,40 +24,60 @@ function fetchURLs() {
         console.log(regexElem,"regex")
         let filtered = regexElem.filter(item => item.length > 0)
         let projects=[]
-        console.log(filtered,"filter brefore first shift")
+        // console.log(filtered,"filter brefore first shift")
 
         filtered.shift()
-        console.log(filtered,"filter after first shift")
+        // console.log(filtered,"filter after first shift")
 
         if(filtered[0]=="Projects"|| (filtered[0]==" Projects")){
           filtered.shift()
         }
-        // let x= document.querySelector('.user-profile-link')
-        // console.log(x.innerText.split(' ').pop().toLowerCase())
-        // console.log(modifiedHref.split('/')[3].toLowerCase())
+        
+
+        //logic for showing multiple projects 
+
         if(document.querySelector('.user-profile-link').innerText.split(' ').pop().toLowerCase() == modifiedHref.split('/')[3].toLowerCase()){
           projects.push(filtered[0])
           filtered.map(project=>{
             
             if(project.includes("  ")){
-              console.log(" space detected")
+              // console.log(" space detected")
               projects.push(project)
             }
           })
-          console.log(" repo of logged in user")
+          // console.log(" repo of logged in user")
         }else{
           filtered.map(project=>{
             // console.log(project)
             projects.push((project.trim().split("   ")[0]))
             
           })
-          console.log("repo of unlogged user")
+          // console.log("repo of unlogged user")
         }
-        console.log(filtered,"filter after if condition")
-        console.log("projects",projects)
+        // console.log(filtered,"filter after if condition")
+        // console.log("projects",projects)
         var issue = document.getElementById(`issue_${issueNumber}_link`)
         // console.log(issueNumber)
-        var p = document.createElement('p')
+        var div = document.createElement('div')
+        issue.append(div)
+        projects.forEach(project=>{
+          var p = document.createElement('p')
+          p.innerText = project
+          p.style.background="#6FCA55"
+          p.style.color = "#ffffff"
+          p.style.border="1px solid grey"
+          p.style.width = "50px"
+          p.style.padding = "2px"
+          // p.style.width = "fit-content"
+          p.style.fontSize = "10px"
+          p.style.borderRadius = "2px"
+          p.style.textAlign =  "center";
+          p.style.display = "inline-block"
+          p.style.margin = "2px"
+          // p.classList.add('btn btn-primary')
+          p.innerText=='Noneyet' ? '' : div.append(p)
+
+        })
 
         // below code was causing the projects to appear
         // console.log(document.querySelector('.user-profile-link').innerText.split(' '))
@@ -68,18 +87,8 @@ function fetchURLs() {
         // x.innerText.split(' ').pop().toLowerCase() == modifiedHref.split('/')[3] ? p.innerText = `${filtered.join(' ').split(' ')[2]}` : p.innerText = `${filtered.join(' ').split(' ')[1]}`
         
 
-        //logic for showing multiple projects
         
-        p.innerText = projects.join("  ")
-        p.style.background="#6FCA55"
-        p.style.color = "#ffffff"
-        p.style.border="1px solid grey"
-        p.style.width = "100px"
-        p.style.fontSize = "10px"
-        p.style.borderRadius = "2px"
-        p.style.textAlign =  "center";
-        // p.classList.add('btn btn-primary')
-        p.innerText=='Noneyet' ? '' : issue.append(p)
+        
         
       })
     })
